e3fa1a5efa871bee86540835295bafce20c4c36f
Kasim <kasimakdg6057@gmail.com>
2021-01-12 22:18:37 +0100

    python assignment

---
+++ forgetting_password.py
@@ -0,0 +1,6 @@
+user_name = (input("Please, enter your first name : ")).title()
+
+if user_name == "Joseph":
+  print("Hello, {}! The password is : W@12".format(user_name))
+else:
+  print("Hello, {}! See you later".format(user_name))
